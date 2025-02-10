38e9d003ceea36a7e9a17015c5ae8f9b82d51007
donarbl <donarablanc@gmail.com>
2025-02-10 15:57:58 +0000

    naming fix in current month anq year

--- web.mjs
+++ web.mjs
@@ -21,10 +21,10 @@ let nextBtn =document.createElement ('button')// it creates the next month butto
     nextBtn.addEventListener('click', ()=>{nextMonthBtn(currentYear, currentMonth)})// adds an event listener to the button 
 
 function nextMonthBtn(year, month){// creating the function to move to the next month 
-    month ++ // moves to the next month
-    if (month > 11){// the year transition past December 
-        month = 0 // loops back to january 
-        year ++ // goes to the next year 
+    currentMonth ++ // moves to the next month
+    if ( currentMonth> 11){// the year transition past December 
+        currentMonth = 0 // loops back to january 
+        currentYear ++ // goes to the next year 
     }
 }
 import { getGreeting, } from "./common.mjs";
