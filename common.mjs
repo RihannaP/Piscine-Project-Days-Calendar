ac98377294088b19acc5761ec5b2d53d5e22ae78
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-02-10 00:55:39 +0000

    Implenting Month Grid

--- common.mjs
+++ common.mjs
@@ -1,5 +1,40 @@
 // This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script. You can delete the contents of the file once you have understood how it works.
 
-export function getGreeting() {
+function getGreeting() {
     return "Hello";
 }
+ 
+// Function to generate calendar grid for a given month
+
+function monthgrid(year, month){
+    let today = new Date();
+    //let dayOne = new Date(year, month, 1).getDay()
+    let LastDate = new Date(year, month+1, 0).getDate()
+    //let dayEnd = new Date(year, month, LastDate ).getDay()
+    let gridArray =[]
+    for(let i=1; i<=LastDate; i++){
+        gridArray.push(i)
+    }
+    return gridArray;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//export{getGreeting, renderMonthTable }
